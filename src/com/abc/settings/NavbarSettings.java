c54a13fad30b9ec414d9fe1a7fb9317fab4d74b4
ezio84 <brabus84@gmail.com>
2017-11-16 10:51:26 +0100

    Rever stock navbar custom features

--- src/com/abc/settings/NavbarSettings.java
+++ src/com/abc/settings/NavbarSettings.java
@@ -31,22 +31,12 @@ import com.android.settings.SettingsPreferenceFragment;
 public class NavbarSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
-    private SwitchPreference mNavbarToggle;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.abc_navbar_settings);
         ContentResolver resolver = getActivity().getContentResolver();
 
-        mNavbarToggle = (SwitchPreference) findPreference("navigation_bar_enabled");
-        boolean enabled = Settings.Secure.getIntForUser(
-                resolver, Settings.Secure.NAVIGATION_BAR_ENABLED,
-                getActivity().getResources().getBoolean(
-                com.android.internal.R.bool.config_showNavigationBar) ? 1 : 0,
-                UserHandle.USER_CURRENT) == 1;
-        mNavbarToggle.setChecked(enabled);
-        mNavbarToggle.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -56,14 +46,9 @@ public class NavbarSettings extends SettingsPreferenceFragment implements
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
-        if (preference == mNavbarToggle) {
-            boolean value = (Boolean) newValue;
-            Settings.Secure.putIntForUser(getActivity().getContentResolver(),
-                    Settings.Secure.NAVIGATION_BAR_ENABLED, value ? 1 : 0,
-                    UserHandle.USER_CURRENT);
-            mNavbarToggle.setChecked(value);
+        /*if (preference == mXy) {
             return true;
-        }
+        }*/
         return false;
     }
 }
