e9aea0dfa9852fbceea3d3410b031d6531744608
ezio84 <brabus84@gmail.com>
2018-01-30 11:09:12 +0100

    Bring back less notification sounds [2/2]

--- src/com/abc/settings/NotificationDrawerSettings.java
+++ src/com/abc/settings/NotificationDrawerSettings.java
@@ -34,6 +34,7 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment imple
 
     private ListPreference mTickerMode;
     private ListPreference mNoisyNotification;
+    private ListPreference mAnnoyingNotification;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -56,6 +57,12 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment imple
         mNoisyNotification.setValue(String.valueOf(mode));
         mNoisyNotification.setSummary(mNoisyNotification.getEntry());
 
+        mAnnoyingNotification = (ListPreference) findPreference("less_notification_sounds");
+        mAnnoyingNotification.setOnPreferenceChangeListener(this);
+        int threshold = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD,
+                30000, UserHandle.USER_CURRENT);
+        mAnnoyingNotification.setValue(String.valueOf(threshold));
     }
 
     @Override
@@ -80,6 +87,11 @@ public class NotificationDrawerSettings extends SettingsPreferenceFragment imple
             int index = mNoisyNotification.findIndexOfValue((String) newValue);
             mNoisyNotification.setSummary(
                     mNoisyNotification.getEntries()[index]);
+            return true;
+        } else if (preference.equals(mAnnoyingNotification)) {
+            int mode = Integer.parseInt(((String) newValue).toString());
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD, mode, UserHandle.USER_CURRENT);
             return true;
         }
 
